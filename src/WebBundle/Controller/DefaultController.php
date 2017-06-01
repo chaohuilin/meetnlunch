69ec9144eab4b0bb010833e71cd96ec24b539418
Jonathan Kauch <kauch.jonathan@gmail.com>
2017-06-01 17:44:29 +0900

    add front page and getting start page

--- src/WebBundle/Controller/DefaultController.php
+++ src/WebBundle/Controller/DefaultController.php
@@ -7,11 +7,20 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
 class DefaultController extends Controller
 {
-    /**
-     * @Route("/")
-     */
-    public function indexAction()
-    {
-        return $this->render('WebBundle:Default:index.html.twig');
-    }
+  /**
+   * @Route("/", name="homepage")
+   */
+  public function indexAction()
+  {
+    // replace this example code with whatever you need
+    return $this->render('WebBundle:homepage:index.html.twig');
+  }
+
+  /**
+   * @Route("/getting_started", name="getting_started")
+   */
+  public function gettingstartedAction()
+  {
+    return $this->render('WebBundle:gettingstarted:index.html.twig');
+  }
 }
